7d4c221ecc891d4a286568ab179dbb8c6b85ee9d
Squeezedcoma <michael.wayner@bryanuniversity.edu>
2022-04-17 01:41:44 -0400

    week4

---
+++ week-4/todo/index.js
@@ -0,0 +1,15 @@
+axios.get("https://api.vschool.io/michael-wayner/list")
+    .then(res => {  
+        for (let i=0; i<res.data.length; i++){
+            const todoList = document.getElementById("todoList");
+            const span = document.createElement("span");
+            if (res.data[i].completed == true){
+                span.innerHTML = res.data[i].title.strike();
+            } else if (res.data[i].completed == false){
+                span.innerHTML = res.data[i].title;
+            }
+            todoList.appendChild(span);
+        }
+    })
+
+.catch(()=> console.log("error"));
